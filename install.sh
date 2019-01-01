6062aaa12449cd6cf6bc2d426e3239e98c23c19c
CHOUIB Chawki <chouibc@gamil.com>
2019-01-01 19:03:50 +0100

    update

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ echo "netoyage"
 make clean
 
 # set PATH so it includes user's private bin directories
-PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
 
 mkdir -p $HOME/bin
 mkdir -p $HOME/.local/bin
 
 echo "ajout de l'executable dans le dossier bin de `echo $USER` "
-cp -i mpsh /home/$USER/bin/
+cp -i mpsh /$HOME/bin/
 
 
 
